4a0b0b58e2f5114dcafea5afe04e212f4500604b
minlwin <lwin.zawmin@gmail.com>
2026-04-07 10:45:53 +0630

    Anonymous API

---
+++ jdc-portal/jdc-portal-api/public-api/src/main/java/com/jdc/portal/anonymous/ClassesApi.java
@@ -0,0 +1,10 @@
+package com.jdc.portal.anonymous;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/anonymous/classes")
+public class ClassesApi {
+
+}
