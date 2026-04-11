82b722ff7434dfdcaac15f255cabfff2edc7d93c
minlwin <lwin.zawmin@gmail.com>
2026-04-11 15:23:50 +0630

    Update Docker Compose

--- jdc-portal/jdc-portal-api/domains/src/main/java/com/jdc/portal/JpaConfiguration.java
+++ jdc-portal/jdc-portal-api/domains/src/main/java/com/jdc/portal/JpaConfiguration.java
@@ -1,12 +1,18 @@
 package com.jdc.portal;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jdc.portal.domains.BaseRepositoryImpl;
 
 @Configuration
 @EnableJpaRepositories(repositoryBaseClass = BaseRepositoryImpl.class)
 public class JpaConfiguration {
 
+	@Bean
+	ObjectMapper objectMapper() {
+		return new ObjectMapper();
+	}
 }
